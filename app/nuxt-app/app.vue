0b90f475d112e15bf20cf23329ec8d0e37381131
Ole Marius Løset <olemariusloset@gmail.com>
2023-05-02 12:18:01 +0200

    fix(nuxt): make auto import component work with nuxt

--- app/nuxt-app/app.vue
+++ app/nuxt-app/app.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import { MyBrandButton } from '@bloc/bloc-components';
 
 </script>
 <template>
--- app/nuxt-app/nuxt.config.ts
+++ app/nuxt-app/nuxt.config.ts
@@ -1,4 +1,7 @@
 import { defineNuxtConfig } from 'nuxt/config'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    components:[
+        '../../packages/bloc-components/src/components',
+    ]
 })
