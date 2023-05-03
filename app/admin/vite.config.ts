6f3cdcbc3d1ef7860491aa4825fa0b28258036a8
Ole Marius Løset <olemariusloset@gmail.com>
2023-05-03 09:38:55 +0200

    fix(demo): ssr test

--- app/admin/vite.config.ts
+++ app/admin/vite.config.ts
@@ -4,21 +4,21 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      dts: true,
-      extensions: ['vue'],
-      dirs: ['src/components', '../../packages/bloc-components/src/components'],
-      deep: true,
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      exclude: ['src/components/Widgets'],
-    }),
-  ],
-  server: {
-    open: true,
-    port: 8003,
-    host: '0.0.0.0',
-    strictPort: true,
-  },
+    plugins: [
+        vue(),
+        Components({
+            dts: true,
+            extensions: ['vue'],
+            dirs: ['src/components', '../../packages/bloc-components/src/components'],
+            deep: true,
+            include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+            exclude: ['src/components/Widgets'],
+        }),
+    ],
+    server: {
+        open: true,
+        port: 8003,
+        host: '0.0.0.0',
+        strictPort: true,
+    },
 });
--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,24 +1,25 @@
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
-
+import ssr from 'vite-plugin-ssr/plugin'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      dts: true,
-      extensions: ['vue'],
-      dirs: ['src/components', '../../packages/bloc-components/src/components'],
-      deep: true,
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      exclude: ['src/components/Widgets'],
-    }),
-  ],
-  server: {
-    open: true,
-    port: 8004,
-    host: '0.0.0.0',
-    strictPort: true,
-  },
+    plugins: [
+        vue(),
+        ssr(),
+        Components({
+            dts: true,
+            extensions: ['vue'],
+            dirs: ['src/components', '../../packages/bloc-components/src/components'],
+            deep: true,
+            include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+            exclude: ['src/components/Widgets'],
+        }),
+    ],
+    server: {
+        open: true,
+        port: 8004,
+        host: '0.0.0.0',
+        strictPort: true,
+    },
 });
